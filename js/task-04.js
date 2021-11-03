260f49dd448cd55a6bdb04cfa6b8e976d015ee6b
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-03 21:43:09 +0100

    task 04 counter
    task 04 counter works

--- js/task-04.js
+++ js/task-04.js
@@ -3,8 +3,10 @@ const buttonInc = document.querySelector("[data-action=increment]")
 buttonDec.addEventListener('click', deButtonClick)
 buttonInc.addEventListener('click', inButtonClick)
 function deButtonClick() {
-    console.log('DeKlick')
+    counter.textContent++
 }
 function inButtonClick() {
-    console.log('InKlick')
+    counter.textContent--
 }
+const counter = document.querySelector('span')
+console.log(counter.textContent)
