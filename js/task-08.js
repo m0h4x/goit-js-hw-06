80d6dfea49434a2e9e93ce4b236b243e3d453cfb
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-08 21:20:41 +0100

    task 08 form
    task 08 email form

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
 
+const inputForm = document.querySelector(".login-form");
+
+inputForm.addEventListener("submit", onFormSubmit);
+
+let result = {};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    let result = {};
+
+    let email = inputForm.elements.namedItem("email").value;
+    let password = inputForm.elements.namedItem("password").value
+
+    if (email === "" || password === "") {
+        alert("Please fill all fields!");
+    }
+
+    if (email && password) {
+        result["email"] = email;
+        result["password"] = password;
+    }
+
+    console.log(result);
+    event.currentTarget.reset();
+}
