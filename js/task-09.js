12398fcdfc11ee965c2d31d0bf188ef03045514d
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-09 22:49:39 +0100

    task 09 color
    task 09 change color

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorButton = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+colorButton.addEventListener('click', updateColor);
+
+function updateColor(event) {
+  let randColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randColor;
+  colorSpan.textContent = randColor;
+}
