96e33e081639cf4ada0bbfdaf83461eb4edeaa92
m0h4x <86525723+m0h4x@users.noreply.github.com>
2021-11-10 22:24:31 +0100

    task 05 if else
    task 05 if else fix

--- js/task-05.js
+++ js/task-05.js
@@ -4,5 +4,9 @@ console.log('inputForm: ', inputForm)
 console.log('outPut: ',outPut.textContent)
 inputForm.addEventListener('input', onFormSubmit);
 function onFormSubmit(event) {
-    outPut.textContent = event.target.value;
+    if (event.target.value == "") {
+        outPut.textContent = "Anonymous";
+    } else {
+        outPut.textContent = event.target.value;
+    }
 }
